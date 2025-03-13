78d6cfd79d196be61b60ddd980574e107148b423
SH YEO <horang@mz.co.kr>
2025-03-13 21:53:26 +0900

    기본세팅1.0,1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,18 @@ java {
 
 repositories {
 	mavenCentral()
-}
+}// build.gradle (Groovy DSL)
+
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")  // JPA 의존성
+	implementation("mysql:mysql-connector-java:8.0.26") // MySQL 8.0+ JDBC 드라이버 (버전 명시)
+	implementation("org.springframework.boot:spring-boot-starter-web")  // 웹 애플리케이션을 위한 의존성 (Optional)
+
+
+	// 테스트 의존성
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/example/horangKt/HorangKtApplication.kt
+++ src/main/kotlin/com/example/horangKt/HorangKtApplication.kt
@@ -1,12 +1,14 @@
 package com.example.horangKt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class HorangKtApplication
 
 fun main(args: Array<String>) {
-
+	println("실행성공")
 	runApplication<HorangKtApplication>(*args)
 }
