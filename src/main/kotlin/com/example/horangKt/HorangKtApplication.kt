8fbc76ffeada412691bcf37d5d3b2ce7aafce8c9
SH YEO <horang@mz.co.kr>
2025-03-13 20:46:27 +0900

    기본세팅

---
+++ src/main/kotlin/com/example/horangKt/HorangKtApplication.kt
@@ -0,0 +1,12 @@
+package com.example.horangKt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HorangKtApplication
+
+fun main(args: Array<String>) {
+
+	runApplication<HorangKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/horangKt/HorangKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.horangKt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HorangKtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
